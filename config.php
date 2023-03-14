6bb70c1e63837fdaa45c1ee86960a769d4e7eff2
Justin K <justin.k@justplayer.de>
2023-03-14 12:13:15 +0100

    feat: Validate signature and payload

--- config.php
+++ config.php
@@ -4,3 +4,4 @@ $dc_webhookurl = "";    // Discord webhook URL to use
 $dc_hiddenchar = "!";   // If commit message is prefixed with this character, it'll hidden it
 $dc_hiddenmsg = "This commit is private."; // Text to show if a commit message is hidden
 $dc_hiddenreps = array();  // An array containing repo names to hide - use short version, not Organization/repository
+$dc_secret = ""; // Secret key to verify payload
